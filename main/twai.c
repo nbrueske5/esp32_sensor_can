284f8853a9cae5a53371d8ac5f541012d57aab46
unknown <nbrueske@wisc.edu>
2026-02-27 20:11:16 -0600

    unpacks sensor messages via queue and callback

--- main/twai.c
+++ main/twai.c
@@ -11,12 +11,12 @@
 
 twai_node_handle_t node_hdl = NULL;
 QueueHandle_t rxQueue_hdl = NULL;
+TaskHandle_t twai_rx_queue_hdl = NULL;
 
 static const char *TAG = "TWAI";
 
 /**
- * Twai RX callbakc function. Takes the twai frame, stores it into a twai message and adds it to the rxQueue
- * use a freeRTOS task to process the data (ex: printRXData() in rtos.c)
+ * Twai RX callback function. Takes the twai frame, stores it into a twai message and adds it to the rxQueue
  */
 static bool twai_rx_cb(twai_node_handle_t handle, const twai_rx_done_event_data_t *edata, void *user_ctx)
 {
@@ -49,8 +49,8 @@ void twai_init_node(void) {
     twai_onchip_node_config_t node_config = {
         .io_cfg.tx = 4,             // TWAI TX GPIO pin
         .io_cfg.rx = 5,             // TWAI RX GPIO pin
-        .bit_timing.bitrate = 1000000,  // 1Mb bitrate
-        .tx_queue_depth = 5,        // Transmit queue depth set to 5
+        .bit_timing.bitrate = 1000000,  // 1Mb
+        .tx_queue_depth = 5,
         .flags = {
             //to enable self testing: 
             //.enable_loopback = 1,
@@ -62,7 +62,6 @@ void twai_init_node(void) {
     ESP_ERROR_CHECK(twai_new_node_onchip(&node_config, &node_hdl));
 
     // configure receive filter
-    // current setup filters out even IDs
     twai_mask_filter_config_t mfilter_cfg = {
     .id = 0,    /**< Single base ID for filtering */
     .mask = 0,      /**< Mask to determine the matching bits (1 = match bit, 0 = any bit) */
@@ -82,12 +81,34 @@ void twai_init_node(void) {
     ESP_ERROR_CHECK(twai_node_enable(node_hdl)); 
     ESP_LOGI(TAG, "created and started the TWAI controller");
 
-    // initiate sensor data decoding
-    sensor_can_data_init(); // TODO, why doesn't this work?
+    sensor_can_data_init();
     
-    // create task to receive can frames from queue
+    // create task to free twai frames from the queue
+    xTaskCreate(
+        twai_rx_task,
+        "twai receive from queue",
+        5000, // stack size -> experimental
+        NULL, // no params
+        4, //PRIORITY -> TODO
+        &twai_rx_queue_hdl
+    );
+}
 
-
+/**
+ * Task responsible for taking twai frames from the rxQueue and unpacking them using the generated sensor can files
+ */
+static void twai_rx_task(void *param) {
+    twai_message_t message_frame;
+    uint8_t message[8] = {0};
+    for(;;) {
+        if (xQueueReceive(rxQueue_hdl, &message_frame, pdMS_TO_TICKS(20)) == pdTRUE) {
+            // unpack the message
+            if (unpack_sensor_message(message_frame.identifier, message_frame.data, message_frame.data_length_code) != 0) {
+                ESP_LOGE(TAG, "Failed unpacking frame from queue: %d", message_frame.identifier);
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(20));
+    }
 }
 
 /**
@@ -115,17 +136,4 @@ esp_err_t twai_transmit(uint32_t id, uint8_t* tx_buff, uint8_t tx_buff_size) {
     return twai_node_transmit(node_hdl, &tx_msg, 0);  // Timeout = 0: returns immediately if queue is full
 }
 
-static void twai_rx_task(void *param) {
-    twai_message_t message_frame;
-    uint8_t message[8] = {0};
-    for(;;) {
-        if (xQueueReceive(rxQueue_hdl, &message_frame, pdMS_TO_TICKS(20)) == pdTRUE) {
-            ESP_LOGI(TAG, "ID: %d, DLC: %d, message: ", message_frame.identifier, message_frame.data_length_code);
-            // unpack the message
-            if (unpack_sensor_message(message_frame.identifier, message_frame.data, message_frame.data_length_code) != 0) {
-                // TODO
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
-}
+
